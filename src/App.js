c319525e1b0995ccfd9bfaceb4e966d45b5182d1
shir <shir8806@gmail.com>
2022-12-14 15:59:25 +0200

    adding Welcome comp, Game comp, router and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Welcome } from './pages/Welcome';
+import { Game } from './pages/Game';
+import { useState } from 'react';
 
 function App() {
+
+  const [page, setPage] = useState(<Welcome/>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Wordle!</h1>
+      <Welcome/>
+      <Game/>
+    </> 
   );
 }
 
---
+++ src/pages/Welcome.js
@@ -0,0 +1,8 @@
+export function Welcome () {
+    return (
+        <>
+            <h1>Welcome User</h1>
+            <a href={'game'} >Start Play</a>
+       </>
+    )
+}   
