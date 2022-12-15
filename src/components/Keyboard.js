cfb3aca454bd57bf5a524f538b0544ebebd2a0cc
shir <shir8806@gmail.com>
2022-12-15 12:15:53 +0200

    create a keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -2,11 +2,22 @@ import React from "react";
 
 export function Keyboard() {
 
-    return (
-        <div className="input-grid">
-            {Array(30).fill().map((_, i) => (
-                <input type="text" key={i} className="input-field" />
-            ))}
+    const keys = [
+        'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+        'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+        'Z', 'X', 'C', 'V', 'B', 'N', 'M', 
+        'Enter', 'Random Guess', 'Del'
+      ];
+    
+      return (
+        <div className="keyboard">
+          {keys.map((key, i) => (
+            key === 'Random Guess' ? <button key={i} className="key" style={{width: '170px'}}>{key}</button> :
+            <button key={i} className="key">{key}</button>
+
+          ))}
         </div>
-  );
-};
+      );
+    };
+
+   
